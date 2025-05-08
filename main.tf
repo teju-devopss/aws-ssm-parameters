b817d282213fff5ef6e3f0283c350b653d93ddfb
tejudevops <saitejaswini8121@gmail.com>
2025-05-08 21:46:15 +0530

    aws-ssm-parameters

--- main.tf
+++ main.tf
@@ -1,17 +1,34 @@
+# resource "aws_ssm_parameter" "parameters" {
+#   for_each = var.parameters
+#   # as "dev-expense.rds.username"(key)= "admin1"(value)
+#   name  = each.key
+#   type  = "String"
+#   value = each.value
+#   key_id = var.key_id # when you store credentails u need a key for security so we are using arn
+# }
+#
+# resource "aws_ssm_parameter" "secrets" {
+#   for_each = var.secrets
+#   # as "dev-expense.rds.password"(key) = "ExpenseApp123"(value)
+#   name  = each.key
+#   type  = "SecureString"
+#   value = each.value
+#   key_id = var.key_id # when you store credentails u need a key for security so we are using arn
+# }
 resource "aws_ssm_parameter" "parameters" {
   for_each = var.parameters
-  # as "dev-expense.rds.username"(key)= "admin1"(value)
-  name  = each.key
-  type  = "String"
-  value = each.value
-  key_id = var.key_id # when you store credentails u need a key for security so we are using arn
+  # Storing general parameters like username, endpoint (non-sensitive)
+  name      = each.key
+  type      = "String"  # 'String' type for non-sensitive data
+  value     = each.value
+  key_id    = var.key_id # For encryption, use KMS key if needed for securing the parameter
 }
 
 resource "aws_ssm_parameter" "secrets" {
   for_each = var.secrets
-  # as "dev-expense.rds.password"(key) = "ExpenseApp123"(value)
-  name  = each.key
-  type  = "SecureString"
-  value = each.value
-  key_id = var.key_id # when you store credentails u need a key for security so we are using arn
+  # Storing sensitive information like passwords (SecureString type)
+  name      = each.key
+  type      = "SecureString"  # 'SecureString' type for sensitive data (e.g., passwords)
+  value     = each.value
+  key_id    = var.key_id # Use KMS encryption key for SecureString
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,34 @@
+# variable "parameters" {
+#   default = {
+#     "dev-expense.rds.username" = "admin1"
+#      "dev-expense.rds.endpoint" = "dev-expense-rds.cqxq6884ocuz.us-east-1.rds.amazonaws.com"
+#
+#   }
+# }
+# ## in organisations we wont keep passwords in git as in training for learning purpose we are keeping here
+# variable "secrets" {
+#   default = {
+#     "dev-expense.rds.password" = "ExpenseApp123"
+#   }
+# }
+# variable "key_id" {
+#   default = "arn:aws:kms:us-east-1:522814736516:key/94568fc4-e087-46db-8d88-a6e69ed61d8e"
+# }
+
 variable "parameters" {
   default = {
     "dev-expense.rds.username" = "admin1"
-     "dev-expense.rds.endpoint" = "dev-expense-rds.cqxq6884ocuz.us-east-1.rds.amazonaws.com"
-
+    "dev-expense.rds.endpoint" = "dev-expense-rds.cqxq6884ocuz.us-east-1.rds.amazonaws.com"
   }
 }
-## in organisations we wont keep passwords in git as in training for learning purpose we are keeping here
+
+## In organizations, we won't keep passwords in git. For learning purposes, we're keeping them here.
 variable "secrets" {
   default = {
     "dev-expense.rds.password" = "ExpenseApp123"
   }
 }
+
 variable "key_id" {
   default = "arn:aws:kms:us-east-1:522814736516:key/94568fc4-e087-46db-8d88-a6e69ed61d8e"
 }
