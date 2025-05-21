adc9fd1f170bd2a59ca3c8ce22e1a95574bb9031
tejudevops <saitejaswini8121@gmail.com>
2025-05-21 13:44:47 +0530

    aws-ssm-parameters

--- variables.tf
+++ variables.tf
@@ -19,6 +19,7 @@ variable "parameters" {
   default = {
     "dev.expense.rds.username" = "admin1"
     "dev.expense.rds.endpoint" = "dev-expense-rds.cqxq6884ocuz.us-east-1.rds.amazonaws.com"
+    "dev.expense.frontend.backend.endpoint"  = "https://backend-dev.tejudevops.online/"
   }
 }
 
