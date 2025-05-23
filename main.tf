deb12b25ea3d62d630a236a862d578f762bba9a9
tejudevops <saitejaswini8121@gmail.com>
2025-05-23 14:05:29 +0530

    aws-ssm-parameters

--- main.tf
+++ main.tf
@@ -1,22 +1,38 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
+# resource "aws_ssm_parameter" "parameters" {
+#   for_each = var.parameters
+#   # as "dev-expense.rds.username"(key)= "admin1"(value)
+#   name  = each.key
+#   type  = "String"
+#   value = each.value
+#   key_id = var.key_id # when you store credentails u need a key for security so we are using arn
+# }
+#
+# resource "aws_ssm_parameter" "secrets" {
+#   for_each = var.secrets
+#   # as "dev-expense.rds.password"(key) = "ExpenseApp123"(value)
+#   name  = each.key
+#   type  = "SecureString"
+#   value = each.value
+#   key_id = var.key_id # when you store credentails u need a key for security so we are using arn
+# }
+# -----------------------------------------------------------------------------------------------
 resource "aws_ssm_parameter" "parameters" {
   for_each = var.parameters
-  # as "dev-expense.rds.username"(key)= "admin1"(value)
-  name  = each.key
-  type  = "String"
-  value = each.value
-  key_id = var.key_id # when you store credentails u need a key for security so we are using arn
+  name     = each.key
+  type     = "String"
+  value    = each.value
+  key_id   = var.key_id
 }
 
 resource "aws_ssm_parameter" "secrets" {
   for_each = var.secrets
-  # as "dev-expense.rds.password"(key) = "ExpenseApp123"(value)
-  name  = each.key
-  type  = "SecureString"
-  value = each.value
-  key_id = var.key_id # when you store credentails u need a key for security so we are using arn
+  name     = each.key
+  type     = "SecureString"
+  value    = each.value
+  key_id   = var.key_id
 }
 # provider "aws" {
 #   region = "us-east-1"
