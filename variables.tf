984f3fca38a776881ff02ffb50ce1af45dad7e21
tejaswiniforaws <tejaswiniforaws@gmail.com>
2025-07-31 16:56:21 +0530

    roboshop clusters setup

--- variables.tf
+++ variables.tf
@@ -25,6 +25,51 @@ variable "parameters" {
     "dev.expense.rds.endpoint" = "dev-expense-rds.cqxq6884ocuz.us-east-1.rds.amazonaws.com"
     "dev.expense.frontend.backend_endpoint"  = "https://backend-dev.tejudevops.online/"
     "newrelic.account" = "6828358"
+
+    # "prod.expense.rds.username"              = "admin1"
+    # "prod.expense.rds.endpoint"              = "prod-expense-rds.cbvsbeoyxek4.us-east-1.rds.amazonaws.com"
+    # "prod.expense.frontend.backend_endpoint" = "https://backend-prod.rdevopsb73.online/"
+    # "prod.expense.frontend.app_version"      = "1.0.1"
+    # "prod.expense.backend.app_version"       = "1.0.1"
+
+    # RoboShop Project
+    # "dev.roboshop.docdb.endpoint"         = "dev-roboshop-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com"
+    # "dev.roboshop.catalogue.MONGO_URL"    = "mongodb://admin1:RoboShop12345@dev-roboshop-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+    # "dev.roboshop.user.MONGO_URL"         = "mongodb://admin1:RoboShop12345@dev-roboshop-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+    # "dev.roboshop.user.REDIS_HOST"        = "dev-roboshop-elasticache.nud0cy.0001.use1.cache.amazonaws.com"
+    # "dev.roboshop.cart.REDIS_HOST"        = "dev-roboshop-elasticache.nud0cy.0001.use1.cache.amazonaws.com"
+    # "dev.roboshop.cart.CATALOGUE_HOST"    = "catalogue"
+    # "dev.roboshop.cart.CATALOGUE_PORT"    = "80"
+    # "dev.roboshop.shipping.CART_ENDPOINT" = "cart"
+    # "dev.roboshop.shipping.DB_HOST"       = "dev-mysql-rds.cbvsbeoyxek4.us-east-1.rds.amazonaws.com"
+    # "dev.roboshop.rds.endpoint"           = "dev-mysql-rds.cbvsbeoyxek4.us-east-1.rds.amazonaws.com"
+    #
+    # "dev.roboshop.payment.CART_HOST" = "cart"
+    # "dev.roboshop.payment.CART_PORT" = "80"
+    # "dev.roboshop.payment.USER_HOST" = "user"
+    # "dev.roboshop.payment.USER_PORT" = "80"
+    # "dev.roboshop.payment.AMQP_HOST" = "rabbitmq-dev.rdevopsb73.online"
+    # "dev.roboshop.payment.AMQP_USER" = "roboshop"
+    # "dev.roboshop.payment.AMQP_PASS" = "roboshop123"
+    #
+    #
+    # "prod.roboshop.docdb.endpoint"         = "prod-roboshop-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com"
+    # "prod.roboshop.catalogue.MONGO_URL"    = "mongodb://admin1:RoboShop12345@prod-roboshop-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+    # "prod.roboshop.user.MONGO_URL"         = "mongodb://admin1:RoboShop12345@prod-roboshop-docdb.cluster-cbvsbeoyxek4.us-east-1.docdb.amazonaws.com:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+    # "prod.roboshop.user.REDIS_HOST"        = "prod-roboshop-elasticache.nud0cy.0001.use1.cache.amazonaws.com"
+    # "prod.roboshop.cart.REDIS_HOST"        = "prod-roboshop-elasticache.nud0cy.0001.use1.cache.amazonaws.com"
+    # "prod.roboshop.cart.CATALOGUE_HOST"    = "catalogue"
+    # "prod.roboshop.cart.CATALOGUE_PORT"    = "80"
+    # "prod.roboshop.shipping.CART_ENDPOINT" = "cart"
+    # "prod.roboshop.shipping.DB_HOST"       = "prod-mysql-rds.cbvsbeoyxek4.us-east-1.rds.amazonaws.com"
+    # "prod.roboshop.rds.endpoint"           = "prod-mysql-rds.cbvsbeoyxek4.us-east-1.rds.amazonaws.com"
+    #
+    # "prod.roboshop.payment.CART_HOST" = "cart"
+    # "prod.roboshop.payment.CART_PORT" = "80"
+    # "prod.roboshop.payment.USER_HOST" = "user"
+    # "prod.roboshop.payment.USER_PORT" = "80"
+    # "prod.roboshop.payment.AMQP_HOST" = "rabbitmq-prod.rdevopsb73.online"
+    # "prod.roboshop.payment.AMQP_USER" = "roboshop"
   }
 }
 
@@ -42,8 +87,26 @@ variable "secrets" {
     "dev.expense.frontend.newrelic.key" = "NRAK-L6VFVUP37CYI82XN1L6UX34UBY0"
     "dev.expense.backend.newrelic.key" = "8a53f9047d104617f4a60b993d87429bFFFFNRAL"
     "elasticsearch.password"           = "VxrrE1BIHbec-H_838jx"
+
+
+    ## RoboShop Project
+    "dev.roboshop.rds.username"      = "admin1"
+    "dev.roboshop.rds.password"      = "RoboShop12345"
+    "dev.roboshop.docdb.username"    = "admin1"
+    "dev.roboshop.docdb.password"    = "RoboShop12345"
+    "dev.roboshop.rabbitmq.username" = "roboshop"
+    "dev.roboshop.rabbitmq.password" = "roboshop123"
+
+    "prod.roboshop.rds.username"      = "admin1"
+    "prod.roboshop.rds.password"      = "RoboShop12345"
+    "prod.roboshop.docdb.username"    = "admin1"
+    "prod.roboshop.docdb.password"    = "RoboShop12345"
+    "prod.roboshop.rabbitmq.username" = "roboshop"
+    "prod.roboshop.rabbitmq.password" = "roboshop123"
+
   }
 }
+
 
 
 #jjn
